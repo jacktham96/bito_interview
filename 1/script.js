81cac3e85f62561b5bdc5a3cf43f07158d85faa9
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2023-02-24 17:01:32 +0800

    Add Class setup

--- 1/script.js
+++ 1/script.js
@@ -1,33 +1,37 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const assert = require('assert');
 
-async function googleSearchTest() {
-  // create a new WebDriver instance
-  const driver = await new Builder().forBrowser('chrome').build();
+async function move_to_bottom(driver) {
+  await driver.sleep(1000);
+  await driver.executeScript("var q=document.documentElement.scrollTop=10000");
+}
 
-  try {
-    // navigate to Google
-    await driver.get('https://www.google.com/');
+class Setup {
+  constructor() {
+    this.search_key = 'BitoPro';
+    this.web_url = 'https://www.google.com/';
+    this.search_title_list = [];
+    this.search_title_url_list = [];
+    this.verify_title_str = 'BitoPro 台灣幣託交易所';
+    this.verify_url_str = 'https://www.bitopro.com';
+    this.driver = new Builder().forBrowser('chrome').build();
+    this.driver.manage().setTimeouts({ pageLoad: 15000 });
+  }
 
-    // find the search box and enter a keyword
-    const searchBox = await driver.findElement(By.name('q'));
-    await searchBox.sendKeys('pornhub', Key.RETURN);
+  async test_google_search_results() {
+    await this.driver.get(this.web_url);
+    const get_search_result = await this.driver.wait(until.elementLocated(By.name('q')), 5000,);
+    await get_search_result.sendKeys(this.search_key, Key.RETURN);
+    
+    await move_to_bottom(this.driver);
 
-    // wait for the search results to load
-    await driver.wait(until.titleContains('selenium webdriver'), 5000);
+  }
 
-    // assert that the search results contain the keyword
-    const resultStats = await driver.findElement(By.id('result-stats'));
-    const resultStatsText = await resultStats.getText();
-    assert.ok(resultStatsText.includes('About'), 'Search results do not contain the keyword');
-
-    console.log('Test passed successfully');
-  } catch (error) {
-    console.error('Test failed: ', error);
-  } finally {
-    // close the browser
-    await driver.quit();
+  async quit() {
+    await this.driver.quit();
   }
 }
 
-googleSearchTest();
+const test = new Setup();
+test.test_google_search_results()
+  .then(() => test.quit());
